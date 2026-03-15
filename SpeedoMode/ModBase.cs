d316ff3f1ae7d57a1bff3fdb4fcafdb0292c8619
ShadowAPI <90363162+ShadowDragon128@users.noreply.github.com>
2026-03-14 19:20:54 -0500

    Add project files.

---
+++ SpeedoMode/ModBase.cs
@@ -0,0 +1,60 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine.InputSystem;
+
+namespace SpeedoMode
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class ModBase : BaseUnityPlugin
+    {
+        private const string pluginGuid = "ShadowAPI.LethalCompany.SpeedoMode";
+        private const string pluginName = "SpeedoMode";
+        private const string pluginVersion = "0.1.0";
+
+        public static ManualLogSource Logger_;
+
+        private void Awake()
+        {
+            Logger.LogInfo("Awake Called");
+            Logger_ = Logger;
+            Logger.LogInfo("SikoMode");
+
+            Harmony harmonyInstance = new Harmony(pluginGuid);
+
+            harmonyInstance.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        private static InputAction action;
+
+        private static bool sprintToggled;
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        private static void UpdateFunc(ref float ___sprintMultiplier, ref bool ___isSprinting, ref bool ___isFallingFromJump)
+        {
+            if (___isSprinting && sprintToggled)
+            {
+                ___sprintMultiplier = 100f;
+            }
+        }
+
+        [HarmonyPatch("Awake")]
+        [HarmonyPostfix]
+        private static void AwakeFunc()
+        {
+            ModBase.Logger_.LogInfo("AwakeFunc Called");
+            action = new InputAction("Toggle", InputActionType.Value, "<Keyboard>/r");
+            action.performed += delegate
+            {
+                sprintToggled = !sprintToggled;
+            };
+            action.Enable();
+        }
+    }
+}
